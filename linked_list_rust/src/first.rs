9fb93e3deec60c7717f84d288145f817407f9361
oabrivard <olivier@abrivard.fr>
2022-09-05 21:44:30 -0400

    Implement various linked list in Rust

---
+++ linked_list_rust/src/first.rs
@@ -0,0 +1,88 @@
+use std::mem;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
+enum Link<T> {
+    Empty,
+    More(Box<Node<T>>),
+}
+
+pub struct List<T> {
+    head: Link<T>
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        // `while let` == "do this thing until this pattern doesn't match"
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+            // boxed_node goes out of scope and gets dropped here;
+            // but its Node's `next` field has been set to Link::Empty
+            // so no unbounded recursion occurs.
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
--- linked_list_rust/src/main.rs
+++ linked_list_rust/src/main.rs
@@ -1,3 +1,13 @@
+use crate::second::List;
+
+mod first;
+mod second;
+mod third;
+
 fn main() {
-    println!("Hello, world!");
+    let mut list = List::new();
+    list.push("olivier");
+    list.push("abrivard");
+    let elem = list.pop().unwrap_or("error");
+    println!("Hello, world {}! ", elem);
 }
